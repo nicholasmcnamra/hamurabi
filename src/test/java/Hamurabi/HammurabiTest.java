479b6db9a2c63fcaab6f42586d1e5b5615243ed5
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-16 23:13:14 -0400

    finished

--- src/test/java/Hamurabi/HammurabiTest.java
+++ src/test/java/Hamurabi/HammurabiTest.java
@@ -7,104 +7,103 @@ import java.util.logging.Handler;
 
 public class HammurabiTest {
 
-//    @Test
-////    public void constructorTest() {
-////        Integer people = 100;
-////        Integer grain = 2800;
-////        Integer landAcres = 1000;
-////        Integer bushelsPerAcre = 19;
-////
-////        Hamurabi hamurabi = new Hamurabi();
-////
-////        Assert.assertEquals(people, hamurabi.getPeople());
-////        Assert.assertEquals(grain, hamurabi.getGrain());
-////        Assert.assertEquals(landAcres, hamurabi.getLandAcres());
-////        Assert.assertEquals(bushelsPerAcre, hamurabi.getBushelsPerAcre());
-////
-////    }
-
-//    @Test
-//    public void askHowManyAcresToBuyTest() {
-//        Integer bushelsPerAcre = 19;
-//        Integer landAcres = 50;
-//
-//        Hamurabi hamurabi = new Hamurabi();
-//
-//        Integer expectedAcresToBuy = landAcres/bushelsPerAcre;
-//        Integer actualAcresToBuy = hamurabi.askHowManyAcresToBuy(bushelsPerAcre, landAcres);
-//
-//        Assert.assertEquals(expectedAcresToBuy, actualAcresToBuy);
-//    }
-//
-//    @Test
-//    public void askHowManyAcresToSellTest() {
-//        Integer acresOwned = 100;
-//        Integer acresToSell = 50;
-//
-//        Hamurabi hamurabi = new Hamurabi();
-//
-//        Integer expectedAcresLeft = acresOwned-acresToSell;
-//        Integer actualAcresLeft = hamurabi.askHowManyAcresToSell();
-//
-//        Assert.assertEquals(expectedAcresLeft, actualAcresLeft);
-//    }
-//
-//    @Test
-//    public void askHowMuchGrainToFeedPeopleTest() {
-//        Integer grainToFeedPeople = 1000;
-//
-//        Hamurabi hamurabi = new Hamurabi();
-//
-//        Integer expectedGrainToFeedPeople = grainToFeedPeople;
-//        Integer actualGrainToFeedPeople = hamurabi.askHowMuchGrainToFeedPeople();
-//
-//        Assert.assertEquals(expectedGrainToFeedPeople, actualGrainToFeedPeople);
-//    }
-//
-//    @Test
-//    public void askHowManyAcresToPlantTest() {
-//        Integer acresToPlant = 500;
-//
-//        Hamurabi hamurabi = new Hamurabi();
-//
-//        Integer expectedAcresToPlant = acresToPlant;
-//        Integer actualAcresToPlant = hamurabi.askHowManyAcresToPlant();
-//
-//        Assert.assertEquals(expectedAcresToPlant, actualAcresToPlant);
-//    }
-
     @Test
     public void plagueDeathsTest() {
+        Integer people = 100;
+        Hamurabi hamurabi = new Hamurabi();
 
+        boolean actual = hamurabi.plagueDeaths(people) <= 50;
+
+        Assert.assertTrue(actual);
     }
 
     @Test
     public void starvationDeathsTest() {
+        Integer people = 100;
+        Integer bushelsFedToPeople = 1500;
+        Hamurabi hamurabi = new Hamurabi();
 
+        Integer expectedStarvationDeath = 25;
+
+        Integer actualStarvationDeath = hamurabi.starvationDeaths(people, bushelsFedToPeople);
+
+        Assert.assertEquals(expectedStarvationDeath, actualStarvationDeath);
+    }
+
+    @Test
+    public void starvationDeathsTest2() {
+        Integer people = 50;
+        Integer bushelsFedToPeople = 900;
+        Hamurabi hamurabi = new Hamurabi();
+
+        Integer expectedStarvationDeath = 10;
+
+        Integer actualStarvationDeath = hamurabi.starvationDeaths(people, bushelsFedToPeople);
+
+        Assert.assertEquals(expectedStarvationDeath, actualStarvationDeath);
     }
 
     @Test
     public void uprisingTest() {
+        Integer people = 100;
+        Integer peopleStarved = 45;
+        Hamurabi hamurabi = new Hamurabi();
 
+        boolean expectedUprising = true;
+        boolean actualUprising = hamurabi.uprising(people, peopleStarved);
+
+        Assert.assertEquals(expectedUprising, actualUprising);
+    }
+
+    @Test
+    public void uprisingTest2() {
+        Integer people = 95;
+        Integer peopleStarved = 30;
+        Hamurabi hamurabi = new Hamurabi();
+
+        boolean expectedUprising = false;
+        boolean actualUprising = hamurabi.uprising(people, peopleStarved);
+
+        Assert.assertEquals(expectedUprising, actualUprising);
     }
 
     @Test
     public void immigrantsTest() {
+        Integer people = 100;
+        Integer acresOwned = 1000;
+        Integer grain = 1500;
+        Hamurabi hamurabi = new Hamurabi();
 
+        Integer expectedImmigrants = 3;
+        Integer actualImmigrants = hamurabi.immigrants(people, acresOwned, grain);
+        Assert.assertEquals(expectedImmigrants, actualImmigrants);
     }
 
     @Test
     public void harvestTest() {
+        Integer acresOwned = 500;
+        Hamurabi hamurabi = new Hamurabi();
 
+        boolean actualHarvest = hamurabi.harvest(acresOwned) <= 3000;
+        Assert.assertTrue(actualHarvest);
     }
 
     @Test
     public void grainEatenByRatsTest() {
+        Integer grain = 1000;
+        Hamurabi hamurabi = new Hamurabi();
 
+        boolean actualEatenByRats = hamurabi.grainEatenByRats(grain) <= 300;
+
+        Assert.assertTrue(actualEatenByRats);
     }
 
     @Test
     public void newCostOfLandTest() {
+        Hamurabi hamurabi = new Hamurabi();
 
+        boolean actualCost = hamurabi.newCostOfLand() >= 17 && hamurabi.newCostOfLand() <= 23;
+
+        Assert.assertTrue(actualCost);
     }
 }
